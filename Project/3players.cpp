b68a20fd53df7a10ebae7248a20e192dd55b5f93
Sander <sander.schaffner@gmail.com>
2014-06-07 17:49:55 +0200

    Begin with Gurobi Part
    Added variables and constraint that straight path is optimum and has cost 1

--- Project/3players.cpp
+++ Project/3players.cpp
@@ -3,9 +3,9 @@
 #include <algorithm>
 #include <iterator> // for ostream_iterator
 
+#include "gurobi_c++.h"
 #include "matrix.h"
 #include "paths.h"
-#include "gurobi_c++.h"
 
 using namespace std;
 
@@ -13,6 +13,8 @@ typedef double var;
 
 /*
  * get edge-number depending on vertex-numbers
+ * short, direct ones: 0: 0<->1, 1: 1<->2, 2: 2<->3, 3: 3<->4, 4: 4<->5
+ * direct from source to target: 5: 0<->3, 6: 1<->4, 7: 2<->5
  */
 int edge(int i, int j){
 	if (i>j){
@@ -39,21 +41,12 @@ int edge(int i, int j){
 		return 7;
 }
 
-/*
- * Get all possible paths for each player in the graph
- */
-/*void getPaths(vector< vector<int>& >&paths[3]){
-	vector<int> tmp;
-	tmp.push_back(0);
-	tmp.push_back(3);
-	paths[0].push_back(tmp);
-}*/
-
 int main(int argc, char *argv[]) {
 	try {
-		GRBEnv env = GRBEnv();
 
-		GRBModel model = GRBModel(env);
+		///////////////////////////////////////////////////////////
+		// Create Graph and find all possible paths for each player
+		///////////////////////////////////////////////////////////
 
 		// 1:
 		// Matrix of graph we want to get PoS
@@ -67,9 +60,58 @@ int main(int argc, char *argv[]) {
 
 		// 2:
 		// Get all possible paths for each player in the graph
-		Paths paths(0,3,6); // source, target, number of nodes in graph
-		paths.getAllPaths(graph);
-		paths.print();
+		Paths paths_p1(0,3,6); // source, target, number of nodes in graph
+		//paths_p1.print();
+		Paths paths_p2(1,4,6); // source, target, number of nodes in graph
+		Paths paths_p3(2,5,6); // source, target, number of nodes in graph
+		paths_p1.getAllPaths(graph);
+		paths_p2.getAllPaths(graph);
+		paths_p3.getAllPaths(graph);
+
+		//////////////////////////////////////////
+		// Define optimization problem with GUROBI
+		//////////////////////////////////////////
+
+		GRBEnv env = GRBEnv();
+
+		GRBModel model = GRBModel(env);
+
+		// Define variables
+		const double lowerBound = 0.001;
+		const double upperBound = GRB_INFINITY;
+		const int n_variables = 8;
+
+		// Create variables
+		// var_number: note x -> node y
+		// short, direct ones: 0: 0<->1, 1: 1<->2, 2: 2<->3, 3: 3<->4, 4: 4<->5
+		// direct from source to target: 5: 0<->3, 6: 1<->4, 7: 2<->5
+		double lb[n_variables];
+		fill_n(lb, n_variables, lowerBound);
+		double ub[n_variables];
+		fill_n(ub, n_variables, upperBound);
+		GRBVar* v_edges = model.addVars(lb, ub, NULL, NULL, NULL, n_variables);
+		model.update();
+
+		// 3:
+		// Inequalities saying that the minimum spanning tree is the line (optimum=={0to1to2to3to4to5})
+		// So direct source to target paths have to be bigger than short ones
+		model.addConstr(v_edges[edge(0,1)] <= v_edges[edge(0,3)]);
+		model.addConstr(v_edges[edge(1,2)] <= v_edges[edge(0,3)]);
+		model.addConstr(v_edges[edge(2,3)] <= v_edges[edge(0,3)]);
+		model.addConstr(v_edges[edge(1,2)] <= v_edges[edge(1,4)]);
+		model.addConstr(v_edges[edge(2,3)] <= v_edges[edge(1,4)]);
+		model.addConstr(v_edges[edge(3,4)] <= v_edges[edge(1,4)]);
+		model.addConstr(v_edges[edge(2,3)] <= v_edges[edge(2,5)]);
+		model.addConstr(v_edges[edge(3,4)] <= v_edges[edge(2,5)]);
+		model.addConstr(v_edges[edge(4,5)] <= v_edges[edge(2,5)]);
+		// Cost of the spanning tree is 1 (optimum has cost one -> result of lp gives directly PoS)
+		GRBLinExpr cost_p1_opt = v_edges[edge(0,1)] + v_edges[edge(1,2)]/2 + v_edges[edge(2,3)]/3;
+		GRBLinExpr cost_p2_opt = v_edges[edge(1,2)]/2 + v_edges[edge(2,3)]/3 + v_edges[edge(3,4)]/2;
+		GRBLinExpr cost_p3_opt = v_edges[edge(2,3)]/3 + v_edges[edge(3,4)]/2 + v_edges[edge(4,5)];
+		model.addConstr(cost_p1_opt + cost_p2_opt + cost_p3_opt == 1);
+
+		// 4:
+		// Inequalities checking that the edges (0,3) (1,4) and (2,5) give a Nash equilibrium
 
 	} catch(GRBException e) {
 		cout << "Error code = " << e.getErrorCode() << endl;
